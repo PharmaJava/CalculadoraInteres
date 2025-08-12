642eea96afb8424226f1820f93f60b9e22dc7afd
PharmaJava <115424255+PharmaJava@users.noreply.github.com>
2025-08-12 14:03:16 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { CompoundInterestCalculator } from './App.js';
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(
-  React.createElement(CompoundInterestCalculator),
-  rootElement
-);
